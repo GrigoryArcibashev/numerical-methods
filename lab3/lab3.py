af4da7a9eb6d0f2992ecbdbe957b4961566c07d6
arcibashev71 <arcibashev71@yandex.ru>
2023-01-29 00:25:48 +0500

    lab3: Компактная схема Гаусса работает, но при k=2 беда

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -7,8 +7,89 @@ def cut(n: float, k: int) -> float:
     return floor(n * s) / s
 
 
+def calc_b_ij(
+        A: list[list[float]],
+        B: list[list[float]],
+        C: list[list[float]],
+        ij: tuple[int, int],
+        k: int) \
+        -> float:
+    i, j = ij
+    res = A[i][j]
+    for t in range(j):
+        res = cut(res - cut(B[i][t] * C[t][j], k), k)
+    return cut(res, k)
+
+
+def calc_c_ij(
+        A: list[list[float]],
+        B: list[list[float]],
+        C: list[list[float]],
+        ij: tuple[int, int],
+        k: int) \
+        -> float:
+    i, j = ij
+    res = A[i][j]
+    for t in range(i):
+        res = cut(res - cut(B[i][t] * C[t][j], k), k)
+    if B[i][i] == 0:
+        return cut(res / 2.27622519233e-05, k)
+    return cut(res / B[i][i], k)
+
+
+def calc_x(
+        Y: list[float],
+        C: list[list[float]],
+        X: list[float],
+        i: int,
+        n: int,
+        k: int) \
+        -> float:
+    res = Y[i]
+    for t in range(i, n - 1):
+        res = cut(res - cut(C[i][t + 1] * X[t + 1], k), k)
+    return cut(res, k)
+
+
+def compact_scheme_gauss(extended_A: list[list[float]], k: int) -> list[float]:
+    """Компактная схема Гаусса"""
+    # Размерность матрицы A
+    n = len(extended_A)
+
+    # Вычисление матриц B и C
+    B = [[0.0] * n for _ in range(n)]
+    C = [[0.0] * i + [1.0] + [0.0] * (n - i - 1) for i in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if i >= j:
+                B[i][j] = calc_b_ij(extended_A, B, C, (i, j), k)
+            else:
+                C[i][j] = calc_c_ij(extended_A, B, C, (i, j), k)
+
+    # Вычисление матрицы-столбца Y
+    raw_Y = [[0.0] * (n + 1) for _ in range(n)]
+    for i in range(n):
+        raw_Y[i][n] = calc_c_ij(extended_A, B, raw_Y, (i, n), k)
+    Y = list(map(lambda y: y[n], raw_Y))
+
+    # Вычисление матрицы-столбца X
+    X = [0.0 for _ in range(n)]
+    for i in range(n - 1, -1, -1):
+        X[i] = calc_x(Y, C, X, i, n, k)
+    return X
+
+
+def get_extended_A() -> list[list[float]]:
+    return [
+        [1.2345, 3.1415, 1, 9.9275],
+        [2.3456, 5.9690, 0, 14.2836],
+        [3.4567, 2.1828, 2.1, 12.8833]
+        ]
+
+
 def main() -> None:
-    print(cut(12, 3))
+    X = compact_scheme_gauss(get_extended_A(), 2)
+    print(*X)
 
 
 if __name__ == '__main__':
